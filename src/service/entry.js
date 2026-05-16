dd8b0c35fcf72667b03be6e639aedd89e8ab2496
Enma586 <enmaflore7421@gmail.com>
2026-05-15 19:12:15 -0600

    se añade serviice

--- src/service/entry.js
+++ src/service/entry.js
@@ -1,1 +1,14 @@
-import Entry from '../models/entry'
+import Entry from '../models/Entry.js';
+
+export const createEntry = async (entryData) => {
+    try {
+        const newEntry = new Entry(entryData);
+        const savedEntry = await newEntry.save();
+        
+        return savedEntry;
+    } catch (error) {
+        // Aquí podrías agregar un logger más adelante (como Pino o Winston)
+        console.error("Error en EntryService:", error);
+        throw error;
+    }
+};
